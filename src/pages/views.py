a4134be97a319c7f75ba89c5c8433ccf481746d9
chdamianos <chdamianos@gmail.com>
2020-12-09 15:06:44 +0100

    at 1:42:23

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,13 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def homepage_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
     return render(request, "home.html", {})
 
+
 def contacts_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
+
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {"my_text": "this is about me",
+                  "my_number": 123,
+                  "my_list": [1, 2, 3],
+                  "this_is_true": True}
+    return render(request, "about.html", my_context)
