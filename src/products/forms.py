84b9c04e5eebc198d915bfbcfa645a4786da6a27
chdamianos <chdamianos@gmail.com>
2020-12-15 16:58:26 +0100

    at 2:41:30

--- src/products/forms.py
+++ src/products/forms.py
@@ -10,8 +10,17 @@ class ProductCreateForm(forms.ModelForm):
                   "description",
                   "price"]
 
+
 class RawProductionForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price =forms.DecimalField()
-    
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+        "placeholder": "Your title"
+    }))
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(attrs={
+                                      "placeholder": "Your description",
+                                      "class": "new-class-name",
+                                      "id": "my-textarea-id",
+                                      "rows": 20,
+                                      "cols": 120
+                                  }))
+    price = forms.DecimalField(initial=199.99)
