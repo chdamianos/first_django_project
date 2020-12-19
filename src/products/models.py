63097a7dae1ae4afbb8d2ec93a9f585ab821a87b
chdamianos <chdamianos@gmail.com>
2020-12-19 18:26:52 +0100

    at 3:01:16

--- src/products/models.py
+++ src/products/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=False, null=False)
     features = models.BooleanField(default=False)  # null=True, default=True
+
+    def get_absolute_url(self):
+        return f"/products/{self.id}/"
