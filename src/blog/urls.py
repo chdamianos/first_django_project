aa47015c96453db155b806123521218a60483a05
chdamianos <chdamianos@gmail.com>
2020-12-22 13:54:37 +0100

    at 3:10:44

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from .views import (
     article_detail_view,
-    article_list_view
+    ArticleListView
 )
 
 app_name = "blog"
 urlpatterns = [
     path('<int:my_id>/', article_detail_view, name='article'),
-    path('', article_list_view, name='article-list'),
+    path('', ArticleListView.as_view(), name='article-list'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ArticleForm
 from .models import Article
 
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    ListView,
+    DeleteView
+)
+
+class ArticleListView(ListView):
+    template_name = 'blog/article_list.html'
+    queryset = Article.objects.all()
+
 # Create your views here.
 def article_list_view(request):
     queryset = Article.objects.all()
