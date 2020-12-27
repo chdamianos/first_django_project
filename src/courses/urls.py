bdc5c427bce7b17e20be1a9191ad2127189b6478
chdamianos <chdamianos@gmail.com>
2020-12-27 12:34:57 +0100

    at 3:30:31

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -9,7 +9,7 @@ app_name = "courses"
 urlpatterns = [
     path('',  CourseView.as_view(template_name="contact.html"), name='courses-list'),
     # path('',  my_fbv, name='courses-list'),
-    # path('<int:id>/', ArticleDetailView.as_view(), name='article'),
+    path('<int:id>/', CourseView.as_view(), name='courses-list'),
     # path('create/', ArticleCreateView.as_view(), name='article-create'),
     # path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
     # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import Course
 # Base View Class = View
+
+
 class CourseView(View):
-    template_name = "about.html"
+    template_name = "courses/course_detail.html"
     # Create your views here.
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id=None, *args, **kwargs):
         # GET method
-        return render(request, self.template_name, {})   
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
     # # to handle forms
     # def post(self, request, *args, **kwargs):
-    #     return render(request, 'about.html', {}) 
+    #     return render(request, 'about.html', {})
+# class CourseDetailView(View):
+#     template_name = "about.html"
+#     # Create your views here.
+#     def get(self, request, *args, **kwargs):
+#         # GET method
+#         return render(request, self.template_name, {})
 # Create your views here.
+
+
 def my_fbv(request, *args, **kwargs):
     return render(request, 'about.html', {})
